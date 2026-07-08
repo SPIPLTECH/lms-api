c0bded05d15672dac70d43ee3813d570448f480e
PrasadKSoftPolynomials <prasadk@softpolynomials.com>
2026-07-08 10:09:59 +0530

    sticky-notes

--- src/modules/courses/course.service.js
+++ src/modules/courses/course.service.js
@@ -43,6 +43,13 @@ const getCourses = async (
   }
   else if (role === "GUEST" || role === "STUDENT") {
   query.where.status = "PUBLISHED";
+  query.include={
+    creator:{
+      select:{
+        name:true,
+      }
+    }
+  }
    }
   return await prisma.course.findMany(query);
 };
